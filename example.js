127a8d4df436bb49aa24141d4f6a1f60805c07ce
Garett Tok Ern Liang <36098015+walnutdust@users.noreply.github.com>
2019-11-12 02:34:35 -0500

    Documentation and pre-commit hooks (#1)
    *Added Prettier, es-lint, and typedoc

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ import {
   or,
   getSpec,
   isValid,
-  explain
+  explain,
 } from './lib/utils';
 import {even, positive, isNumber, isString} from './lib/preds';
 import Pred from './lib/spec/pred';
@@ -17,44 +17,52 @@ import {performance} from 'perf_hooks';
 /* eslint-disable no-console */
 
 // Invalid validity checking returns false.
-console.log(isValid(1, even)); // false
+isValid(1, even); // false
 
 // Valid values returns true.
-console.log(isValid(2, even)); // true
+isValid(2, even); // true
 
 // We can define our own specifications.
 define('is-a?', new Pred("is 'a'?", (value) => value === 'a'));
-console.log(isValid('b', 'is-a?')); // false
-console.log(isValid('a', 'is-a?')); // true
+isValid('b', 'is-a?'); // false
+isValid('a', 'is-a?'); // true
 
 // We can join multiple predicates.
 // Note that the definition key does not have to be the same as the explanatory string.
 define('pos-even', and('positive, even number', positive, even));
-console.log(isValid(2, 'pos-even')); // true
-console.log(isValid(-2, 'pos-even')); // false
+isValid(2, 'pos-even'); // true
+isValid(-2, 'pos-even'); // false
 
 // We can also check maps.
 const point = {
   x: 1,
-  y: 2
+  y: 2,
 };
 
 const threeDpoint = new Map({
   x: 1,
   y: 2,
-  z: 3
+  z: 3,
 });
 define('point', keys('2d or 3d point', {x: isNumber, y: isNumber, z: optional(isNumber)}));
 define('point or string', or('point or string', getSpec('point'), isString));
-console.log(isValid(point, 'point')); // true
-explain(point, 'point'); // [object Object]: passes specificiation 2d or 3d point.
-console.log(isValid(threeDpoint, 'point')); // true
-console.log(isValid(threeDpoint, 'point or string')); // true
+isValid(point, 'point'); // true
+explain(point, 'point');
+/*
+  Value:
 
-// We can assert the value, which throws an error if it fails.
+  { x: 1, y: 2 }
+
+  Passes specification 2d or 3d point.
+*/
+
+isValid(threeDpoint, 'point'); // true
+isValid(threeDpoint, 'point or string'); // true
+
+// We can assert the value, which returns invalid if it fails.
 const invalidPoint = new Map({
   x: 'a',
-  y: 2
+  y: 2,
 });
 isValid(invalidPoint, 'point or string'); // false
 // Why did it fail?
@@ -72,12 +80,13 @@ explainIfInvalid(point, 'point or string'); // no output.
 const nestedPoint = new Map({
   x: new Map({
     x: new Map({
-      x: 'hi'
-    })
-  })
+      x: 2,
+      y: 3,
+    }),
+  }),
 });
 define('2-nested point', keys('nested nested point', {
-  x: keys('nested point', {x: getSpec('point')})
+  x: keys('nested point', {x: getSpec('point')}),
 }));
 explain(nestedPoint, '2-nested point');
 // key x->key x->key x->is a number?: hi failed specification.
@@ -88,12 +97,12 @@ const nestedPoint2 = fromJS({
   x: {
     x: {
       x: 2,
-      y: 'hi'
-    }
-  }
+      y: 3,
+    },
+  },
 });
 
-isValid(nestedPoint2, '2-nested point');
+isValid(nestedPoint2, '2-nested point'); // true
 explain(nestedPoint2, '2-nested point');
 
 // This works with normal JS Maps as well.
@@ -101,12 +110,12 @@ const nestedPoint3 = {
   x: {
     x: {
       x: 2,
-      y: 3
-    }
-  }
+      y: 3,
+    },
+  },
 };
 
-isValid(nestedPoint3, '2-nested point');
+isValid(nestedPoint3, '2-nested point'); // true
 explain(nestedPoint3, '2-nested point');
 
 // What of the speed of the comparisons?
@@ -125,8 +134,8 @@ console.log(
   `Test 1: Time taken to perform ${numTimes} validity checks on the JS Map is ${end -
     start}ms, averaging ${(end - start) / numTimes}ms.`
 );
-/* Test 1: Time taken to perform 10000000 validity checks on the JS Map is 3281.227928996086ms, 
-   averaging 0.00032812279289960863ms. */
+/* Test 1: Time taken to perform 10000000 validity checks on the JS Map is 5441.707319021225ms, 
+   averaging 0.0005441707319021225ms.. */
 
 // Test 2: fromJS (immutableJS) Map
 start = performance.now();
@@ -138,8 +147,8 @@ console.log(
   `Test 2: Time taken to perform ${numTimes} validity checks on the fromJS Map is ${end -
     start}ms, averaging ${(end - start) / numTimes}ms.`
 );
-/* Test 2: Time taken to perform 10000000 validity checks on the fromJS Map is 372.3540229797363ms, 
-   averaging 0.00003723540229797363ms. */
+/* Test 2: Time taken to perform 10000000 validity checks on the fromJS Map is 1474.4121170043945ms, 
+   averaging 0.00014744121170043946ms. */
 
 // Test 3: immutableJS Map
 start = performance.now();
@@ -148,12 +157,12 @@ for (let i = 0; i < numTimes; i++) {
 }
 end = performance.now();
 console.log(
-  `Test 3: Time taken to perform ${numTimes} validity checks on the fromJS Map is ${end -
+  `Test 3: Time taken to perform ${numTimes} validity checks on the immutable Map is ${end -
     start}ms, averaging ${(end - start) / numTimes}ms.`
 );
 
-/* Test 3: Time taken to perform 10000000 validity checks on the fromJS Map is 375.91091895103455ms, 
-   averaging 0.00003759109189510345ms. */
+/* Test 3: Time taken to perform 10000000 validity checks on the immutable Map is 1248.7203179597855ms, 
+   averaging 0.00012487203179597855ms. */
 
 /* Notes: As we may have expected, the immutable Maps take about the same times. We also note that 
    the mutable version takes nearly three times as long as the immutable versions with this input. 
